2ed1d39600755a61f0217b1d054bfa2cc39adec0
FuhadSanin <fuhadsanin500@gmail.com>
2023-11-14 16:01:12 +0530

    sani resoponsive

--- components/Fixture/Fixture.jsx
+++ components/Fixture/Fixture.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import "./Fixture.css";
-import ashwa from "../../assets/color-ashwa.png";
-import illus from "../../assets/Football Player.gif";
-import Row from "./Row";
+import React, { useEffect, useRef } from "react"
+import "./Fixture.css"
+import ashwa from "../../assets/color-ashwa.png"
+import illus from "../../assets/Football Player.gif"
+import Row from "./Row"
 
 const Fixture = () => {
+  const containerRef = useRef(null)
+
+  useEffect(
+    () => {
+      // Scroll to the bottom when the component mounts or when content changes
+      scrollToBottom()
+    },
+    [
+      /* dependencies that trigger a scroll, if any */
+    ]
+  )
+
+  const scrollToBottom = () => {
+    if (containerRef.current) {
+      // Scroll to the bottom of the container
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
+    }
+  }
   return (
     <d id="fixture">
       <div className="container">
@@ -17,7 +35,7 @@ const Fixture = () => {
             <h1>23rd WEDNESDAY</h1>
           </div>
         </div>
-        <div className="fixture-list">
+        <div className="fixture-list" ref={containerRef}>
           <Row
             title="Volleyball"
             roundno="Round 1"
@@ -57,11 +75,41 @@ const Fixture = () => {
             left_color="#4DB657"
             right_team="YELLOW"
             right_color="#DBD355"
-          /> 
+          />
+          <Row
+            title="Basketball-"
+            roundno="Round 1"
+            time="11:30"
+            venue="open court"
+            left_team="GREEN"
+            left_color="#4DB657"
+            right_team="YELLOW"
+            right_color="#DBD355"
+          />{" "}
+          <Row
+            title="Basketball-"
+            roundno="Round 1"
+            time="11:30"
+            venue="open court"
+            left_team="GREEN"
+            left_color="#4DB657"
+            right_team="YELLOW"
+            right_color="#DBD355"
+          />{" "}
+          <Row
+            title="Basketball-"
+            roundno="Round 1"
+            time="11:30"
+            venue="open court"
+            left_team="GREEN"
+            left_color="#4DB657"
+            right_team="YELLOW"
+            right_color="#DBD355"
+          />
         </div>
       </div>
     </d>
-  );
-};
+  )
+}
 
-export default Fixture;
+export default Fixture
